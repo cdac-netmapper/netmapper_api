e9d6df492a520005f7d7e1103f114fce4447b900
Hoang Son Pham <pshoang102@gmail.com>
2021-08-24 17:04:25 +0700

    fix readFile examples.json

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require("express")
 const app = express()
-const axios = require("axios").default;
-const { exec } = require("child_process");
+const axios = require("axios").default
+const { exec } = require("child_process")
 const fs = require("fs")
 const util = require("util")
 const readFile = util.promisify(fs.readFile)
 const openFile = util.promisify(fs.open)
 const closeFile = util.promisify(fs.close)
 const writeFile = util.promisify(fs.writeFile)
-const deleteFile = util.promisify(fs.unlink);
-const execCmd = util.promisify(exec);
+const deleteFile = util.promisify(fs.unlink)
+const execCmd = util.promisify(exec)
 const { writeToCsv } = require("./utils/csvHelper")
 
 const apiUrl =
-  "https://nu5rdmmh11.execute-api.us-east-1.amazonaws.com/nm/upload-mngd/";
+  "https://3ccbm8fqq6.execute-api.us-east-2.amazonaws.com/nm/upload-mngd/"
 const challenge =
   "ee07111f94cdb8b0955590b63a71c33e197c2ace6d2b7903b12a7ca35bba1a80" //managed1
 
@@ -31,7 +31,8 @@ app.get("/", (_, res) => {
 /* Get examples */
 app.get("/examples", async (_, res, next) => {
   try {
-    const examples = await JSON.parse(readFile("examples.json"))
+    const examplesFile = await readFile("examples.json")
+    const examples = JSON.parse(examplesFile)
     res.status(200).json({
       statusCode: 200,
       examples,
@@ -78,23 +79,23 @@ app.post("/submit", async (req, res, next) => {
     // POST request to get Pre-signed S3 URL
     const { status, data } = await axios.post(apiUrl + device, input)
     if (status === 200) {
-      const { message, url } = data;
-      console.log("message: ", message);
-      console.log("Pre-signed URL: ", url);
+      const { message, url } = data
+      console.log("message: ", message)
+      console.log("Pre-signed URL: ", url)
       // PUT request to upload file to Pre-signed S3 URL
       const { stdout, stderr } = await execCmd(
         `curl -X PUT -T ${input.file} \'${url}\' -v`
-      );
-      console.log("stdout: ", stdout);
-      console.log("stderr: ", stderr);
+      )
+      console.log("stdout: ", stdout)
+      console.log("stderr: ", stderr)
       // Delete file on completion
       await deleteFile(input.file)
       
       const response = {
         statusCode: status,
         message: `Submitted at ${submissionTime.toUTCString()}`,
-      };
-      res.status(200).json(response);
+      }
+      res.status(200).json(response)
     } else {
       throw Error("Unable to fetch Pre-signed S3 URL!")
     }
